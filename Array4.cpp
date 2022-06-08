dde6353f0288c00f7fabb5c7b4c5fd4d696c2047
piebargallo <90567646+piebargallo@users.noreply.github.com>
2022-06-08 18:54:11 +0200

    Add files via upload

---
+++ Array4.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// driver program
+int main() {
+	
+	// First method 
+	int arr[6] = {11, 12, 13, 14, 15, 16};
+	// Way 1
+	for(int i = 0;i < 6;i++)
+		cout << arr[i] << " ";
+		
+	cout << endl << "\n";
+	
+	// Second method
+	cout << "By Other Method:" << endl;
+	// Way 2
+	for(int i = 0;i < 6;i++)
+		cout << i[arr] << " ";
+
+	cout << endl;
+
+	return 0;
+}// end driver
+
+
+
